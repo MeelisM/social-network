dae8c1fd08450f152aee0f8ae6ed0d88aba27b04
Meelis Mumm <73930233+MeelisM@users.noreply.github.com>
2024-11-24 16:37:48 +0200

    added post comments

--- internal/model/post.go
+++ internal/model/post.go
@@ -3,14 +3,15 @@ package model
 import "time"
 
 type Post struct {
-	ID        string    `json:"id"`
-	UserID    string    `json:"user_id"`
-	Content   string    `json:"content"`
-	ImagePath *string   `json:"image_path,omitempty"`
-	Privacy   string    `json:"privacy"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	ViewerIDs []string  `json:"viewer_ids,omitempty"`
+	ID        string        `json:"id"`
+	UserID    string        `json:"user_id"`
+	Content   string        `json:"content"`
+	ImagePath *string       `json:"image_path,omitempty"`
+	Privacy   string        `json:"privacy"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
+	ViewerIDs []string      `json:"viewer_ids,omitempty"`
+	Comments  []PostComment `json:"comments,omitempty"`
 }
 
 type CreatePostInput struct {
@@ -25,4 +26,15 @@ type UpdatePostInput struct {
 	ImagePath *string   `json:"image_path,omitempty"`
 	Privacy   *string   `json:"privacy,omitempty"`
 	ViewerIDs *[]string `json:"viewer_ids,omitempty"`
+}
+
+type PostComment struct {
+	ID           string    `json:"id"`
+	PostID       string    `json:"post_id"`
+	UserID       string    `json:"user_id"`
+	Content      string    `json:"content"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	UserNickname string    `json:"user_nickname,omitempty"`
+	UserAvatar   string    `json:"user_avatar,omitempty"`
 }
